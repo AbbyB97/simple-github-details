bf7feaf0faa4b7aaeab46696f941726d916e6d97
Abhijit <abhijit.bansode@learnq.ai>
2022-06-28 20:29:07 +0530

    added code changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import React from 'react';
-import './styles.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./styles.css";
 
 function App() {
+  const [repoData, setRepoData] = useState(null);
+  const [formData, setFormData] = useState({
+    userName: null,
+    includeForks: false,
+  });
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${formData.userName}/repos${
+          formData?.includeForks ? "?type=owner" : ""
+        }`
+      );
+      console.log("response", response);
+
+      const data = response?.data;
+      console.log("data", data);
+      setErrorMessage("");
+
+      setRepoData(data);
+    } catch (error) {
+      setErrorMessage("Invalid user name");
+      setRepoData(null);
+    }
+  };
+
+  console.log("repoData", repoData);
   return (
     <div className="App">
       <div className="input">
         <label htmlFor="username">Github username: </label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          onChange={(e) =>
+            setFormData({ ...formData, userName: e.target.value })
+          }
+        />
         <label htmlFor="fork">Include forks: </label>
-        <input id="fork" type="checkbox" />
-        <button>Submit</button>
+        <input
+          id="fork"
+          type="checkbox"
+          onClick={() =>
+            setFormData({ ...formData, includeForks: !formData.includeForks })
+          }
+        />
+        <button
+          disabled={formData.userName === null || ""}
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
       </div>
       <section>
         <header>
@@ -18,14 +66,18 @@ function App() {
           <div className="col">Description</div>
           <div className="col">Size</div>
         </header>
-        <div>
-          <div className="col">C1</div>
-          <div className="col">C2</div>
-          <div className="col">C3</div>
-          <div className="col">C4</div>
-        </div>
+        {repoData?.map((repo, i) => (
+          <React.Fragment key={i}>
+            <div>
+              <div className="col">{repo?.name || "NA"}</div>
+              <div className="col">{repo?.language || "NA"}</div>
+              <div className="col">{repo?.description || "NA"}</div>
+              <div className="col">{repo?.size || "NA"}</div>
+            </div>
+          </React.Fragment>
+        ))}
       </section>
-      <div className="error">Dummy error</div>
+      <div className="error">{errorMessage}</div>
     </div>
   );
 }
